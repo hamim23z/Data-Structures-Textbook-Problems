b79a1ae4b5682f660476bd864c89b35dc28feaf8
hamim23z <hamimc232@gmail.com>
2024-06-24 14:33:25 -0400

    Section 2.3 Problems

---
+++ Chapter2/Section2.3/mainnum18.cpp
@@ -0,0 +1,20 @@
+/*
+Design and implement a class called circle_location to keep track of 
+the position of a single point that travels around a circle. An object 
+of this class records the position of the point as an angle, measured 
+in a clockwise direction from the top of the circle. Include these public 
+member functions:
+• A default constructor to place the point at the top of the circle.
+• Another constructor to place the point at a specified position.
+• A function to move the point a specified number of degrees around the 
+circle. Use a positive argument to move clockwise, and a negative argument
+to move counterclockwise.
+• A function to return the current position of the point, in degrees, measured
+clockwise from the top of the circle.
+
+Your solution should include a separate header file, implementation file, 
+and an example of a main program using the new class.
+*/
+
+
+// EXAMPLE MAIN PROGRAM USING CLASS
---
+++ Chapter2/Section2.3/number19.cpp
@@ -0,0 +1,13 @@
+/*
+Design and implement a class called clock. A clock object holds one instance
+of a time value such as 9:48 P.M. Have at least these public member functions:
+• A default constructor that sets the time to midnight
+• A function to explicitly assign a given time (you will have to give some thought
+to appropriate parameters for this function)
+• Functions to retrieve information: the current hour, the current minute, and a 
+boolean function to determine whether the time is at or before noon
+• A function to advance the time forward by a given number of minutes
+*/
+
+
+// IMPLEMENTATION FILE
